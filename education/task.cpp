cf4ab15ec6ded64450f0d8f94cfa93693e3e8baf
Dzmitry Kamarouski <dzkamarouski@gmail.com>
2018-01-24 01:43:19 +0300

    Update task.cpp

--- education/task.cpp
+++ education/task.cpp
@@ -1,10 +1,7 @@
-#include <opencv2/opencv.hpp>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
-#include <bits/stdc++.h>
-std::set<std::pair<int,int>> checked;
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
 
 void procces(const cv::Mat & in, cv::Mat & mask, std::vector<std::pair<int,int>> pts)
 {
@@ -12,18 +9,22 @@ void procces(const cv::Mat & in, cv::Mat & mask, std::vector<std::pair<int,int>>
     std::vector<int> tmp = {1,-1, 2, -2};
     for(auto point : pts)
     {
-//        if (mask.at<bool>())
+        if (mask.at<bool>(point.first,point.second))
+        {
+//            std::cout << "YYYYYYYYYYYYYYYYYy" << std::endl;
+            continue;
+        }
         for (int i  = 0; i < tmp.size(); ++i)
         {
-            double delta =( int(in.at<uchar>(point.first,point.second) - in.at<uchar>(point.first + tmp[i]/2,point.second + tmp[i]%2) ));
-            if (delta < 5 && checked.find({point.first + tmp[i]/2,point.second + tmp[i]%2})==checked.end())
+            if (point.first + tmp[i]/2 >= in.cols+1 || point.second + tmp[i]%2 >= in.rows+1 || point.first + tmp[i]/2 <= 0 || point.second + tmp[i]%2 <= 0)
+                continue;
+            int delta = std::abs(( int(in.at<uchar>(point.first,point.second)) - int(in.at<uchar>(point.first + tmp[i]/2,point.second + tmp[i]%2) )));
+            mask.at<uchar>({point.second,point.first}) = 255;
+            if (delta < 10 &&  !mask.at<bool>(point.first + tmp[i]/2,point.second + tmp[i]%2))
             {
                 std::cout << "delta =" << delta << std::endl;
-                std::cout << point.first << " " << point.second << " " << point.first + tmp[i]/2 << " " << point.second + tmp[i]%2  << std::endl;
-                mask.at<uchar>({point.first + tmp[i]/2,point.second + tmp[i]%2}) = 255;
+//                std::cout << point.first << " " << point.second << " " << point.first + tmp[i]/2 << " " << point.second + tmp[i]%2  << std::endl;
                 newPts.push_back({point.first + tmp[i]/2,point.second + tmp[i]%2});
-                checked.insert({point.first + tmp[i]/2,point.second + tmp[i]%2});
-
             }
         }
     }
@@ -34,28 +35,26 @@ void procces(const cv::Mat & in, cv::Mat & mask, std::vector<std::pair<int,int>>
         procces(in,mask,newPts);
 
 }
+
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello" << std::endl;
-    cv::Mat img = cv::imread("/home/please/work/unet_segment/train/1.png",0);
+    cv::Mat img = cv::imread("/home/davinci/work/unet_segment/train/1.png",0);
     cv::Mat mask;
-    cv::threshold(img,mask,135,255,cv::THRESH_BINARY);
+    cv::threshold(img,mask,140,255,cv::THRESH_BINARY);
+    cv::imshow("img",img);
+    cv::imshow("mask",mask);
     std::vector<std::pair<int,int>> pts;
     for (int i = 0; i < img.rows; ++i)
     {
         for (int j = 0; j < img.cols; ++j)
         {
-            if (mask.at<double>(i,j))
-            {
+            if (mask.at<bool>(i,j))
                 pts.push_back({i,j});
-                checked.insert({i,j});
-            }
         }
     }
-    procces(img,mask,pts);
-    cv::imshow("mask",mask);
-    cv::imshow("im",img);
-
+    cv::Mat newMask(cv::Size(img.cols,img.rows),mask.type());
+    procces(img,newMask,pts);
+    cv::imshow("newmask",newMask);
     cv::waitKey();
     return 0;
 }
